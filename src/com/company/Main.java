78b239482c4ace1143e481cf243a6bc48f14d002
gizem <gizemsacal@gmail.com>
2021-05-10 16:15:49 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.EnumMap;
+
+class Main {
+
+    enum Size {
+        SMALL, MEDIUM, LARGE, EXTRALARGE
+    }
+    public static void main(String[] args) {
+
+        // Creating an EnumMap of the Size enum
+        EnumMap<Size, Integer> sizes = new EnumMap<>(Size.class);
+        sizes.put(Size.SMALL, 28);
+        sizes.put(Size.MEDIUM, 32);
+        sizes.put(Size.LARGE, 36);
+        sizes.put(Size.EXTRALARGE, 40);
+        System.out.println("EnumMap: " + sizes);
+
+        // Using the get() Method
+        int value = sizes.get(Size.MEDIUM);
+        System.out.println("ORTA DEĞER: " + value);
+    }
+}
